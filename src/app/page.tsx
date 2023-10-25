21bc7c083f31aa218d17ea460ba599876ae6348d
LONEWOLF143 <twaha2030@gmail.com>
2023-10-25 15:05:00 +0300

    new and home pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
-import Image from 'next/image'
+import Link from "next/link";
+import { prisma } from "./db";
+import TodoItem from "@/components/TodoItem";
 
-export default function Home() {
+function getTodos() {
+  return      prisma.todo.findMany()
+
+}
+
+
+export default async function Home() {
+  const todos = await getTodos()
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hello prisma
-    </main>
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">Tod</h1>
+        <Link href="/new"  className=" border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none">New</Link>
+      </header>
+      
+      <ul className="pl-4">
+        {todos.map(todo=> (
+          <TodoItem key={todo.id} {...todo} />
+        ))}
+      </ul>
+    </>
   )
 }
